d230d9fe2ef9a12c8e35dd1e0482cf90b3cc6001
Amauri Santos <amaurisantos.m@gmail.com>
2019-02-04 08:02:21 +0000

    add mongolab

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ const commentRoutes    = require("./routes/comments"),
       indexRoutes      = require("./routes/index");
       
       
-mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true});
+mongoose.connect("mongodb://amauri:amauri12@ds121455.mlab.com:21455/amauri_yelpcamp",{useNewUrlParser: true});
+//mongodb://amauri:amauri12@ds121455.mlab.com:21455/amauri_yelpcamp
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
